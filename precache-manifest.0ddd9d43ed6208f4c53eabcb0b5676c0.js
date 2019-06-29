d381005c32e5bf8538fcb5a9723f3320dd751266
allsports <allsports.developer@gmail.com>
2019-06-29 09:57:57 +0000

    Updates

---
+++ precache-manifest.0ddd9d43ed6208f4c53eabcb0b5676c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5e1644a2a1768a198027fdf182a78dd",
+    "url": "/allsports/index.html"
+  },
+  {
+    "revision": "d2f8684f44561ff4c028",
+    "url": "/allsports/static/css/main.65139417.chunk.css"
+  },
+  {
+    "revision": "4e0e292814ba4c5badbe",
+    "url": "/allsports/static/js/2.c9984f45.chunk.js"
+  },
+  {
+    "revision": "d2f8684f44561ff4c028",
+    "url": "/allsports/static/js/main.f8373a66.chunk.js"
+  },
+  {
+    "revision": "2ea62912ce502620fe54",
+    "url": "/allsports/static/js/runtime~main.e333ad50.js"
+  }
+]);
